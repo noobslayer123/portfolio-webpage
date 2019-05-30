82c66cb7467a7974289799d0312957fdc571db5c
noobslayer123 <stas.snow@gmail.com>
2019-05-30 22:49:19 +0200

    HTML/CSS code refactored, added new animations/features/mobile menu.

---
+++ main.js
@@ -0,0 +1,29 @@
+$("a.nav-btn").on("click", function(e) {
+  if (this.hash !== "") {
+    e.preventDefault();
+
+    const hash = this.hash;
+
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top
+      },
+      800
+    );
+  }
+});
+
+$(".button").on("click", function(e) {
+  if (this.hash !== "") {
+    e.preventDefault();
+
+    const hash = this.hash;
+
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top
+      },
+      800
+    );
+  }
+});
