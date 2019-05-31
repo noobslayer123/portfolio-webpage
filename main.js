622073dd34c878fe85c2eb2eb2ad49fd7fb9efd5
noobslayer123 <stas.snow@gmail.com>
2019-05-31 18:56:19 +0200

    Scroll animation change, style improvements

--- main.js
+++ main.js
@@ -27,3 +27,18 @@ $(".button").on("click", function(e) {
     );
   }
 });
+
+$("a.nav-link-sm").on("click", function(e) {
+  if (this.hash !== "") {
+    e.preventDefault();
+
+    const hash = this.hash;
+
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top
+      },
+      800
+    );
+  }
+});
